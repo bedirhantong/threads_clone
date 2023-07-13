2c038035c5c8587a290380dd22faf0197a8763d1
Bedirhan Tonğ <20200808033@ogr.akdeniz.edu.tr>
2023-07-14 00:17:30 +0300

    issue : Page navigation with bottom navbar issue is solved,explained the solution and closed it on Github Issues

--- lib/src/features/authentication/screens/main_screen/main_screen.dart
+++ lib/src/features/authentication/screens/main_screen/main_screen.dart
@@ -14,29 +14,19 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
+
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
       'Index 0: Home',
       style: optionStyle,
     ),
-    Text(
-      'Index 1: Search',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Post',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Notifications',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 4: Profile',
-      style: optionStyle,
-    ),
+    SearchScreen(),
+    PostScreen(),
+    NotificationsScreen(),
+    ProfileScreen(),
   ];
 
   @override
@@ -55,7 +45,7 @@ class _MainScreenState extends State<MainScreen> {
           centerTitle: true,
         ),
         body: Container(
-          child: MainScreen(),
+          child: _widgetOptions.elementAt(_selectedIndex),
         ),
         bottomNavigationBar: buildBottomNavigationBar(),
       ),
@@ -63,37 +53,9 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   void _onItemTapped(int index) {
-    setState(
-      () {
-        _selectedIndex = index;
-        if (index == 0) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MainScreen()),
-          );
-        } else if (index == 1) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SearchScreen()),
-          );
-        } else if (index == 2) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => PostScreen()),
-          );
-        } else if (index == 3) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => NotificationsScreen()),
-          );
-        } else if (index == 4) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ProfileScreen()),
-          );
-        }
-      },
-    );
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   BottomNavigationBar buildBottomNavigationBar() {
